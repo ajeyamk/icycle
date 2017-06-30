b841944addb83f334422026fbd59290b0ccf4ba6
ajeya <ajeya.kempegowda@ymedialabs.com>
2017-07-01 04:20:17 +0530

    Changed local.conf

--- main/settings/local.py
+++ main/settings/local.py
@@ -34,9 +34,17 @@ HAYSTACK_CONNECTIONS = {
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'icycle_db',
+        'USER': 'icycle_db_user',
+        'PASSWORD': 'icycle_db_password',
+        'HOST': 'icycle.cylwbz3rz1yz.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
