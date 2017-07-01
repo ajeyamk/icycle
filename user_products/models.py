7570b705648506dd456bd80e927c8999aca7c4e0
ajeya <ajeya.kempegowda@ymedialabs.com>
2017-07-01 08:27:30 +0530

    validation check for assign api

--- user_products/models.py
+++ user_products/models.py
@@ -3,6 +3,11 @@ from products.models import Products
 
 
 class UserProducts(models.Model):
+    PRODUCT_STATUS = (
+        (1, 'In use'),
+        (2, 'Returned'),
+        (3, 'Redeemed')
+    )
     user = models.ForeignKey(
         'appauth.AppUser',
         related_name='user_product'
@@ -10,6 +15,9 @@ class UserProducts(models.Model):
     product = models.ForeignKey(
         Products,
         related_name='product'
+    )
+    status = models.IntegerField(
+        choices=PRODUCT_STATUS
     )
     added_on = models.DateTimeField(
         auto_now_add=True
@@ -19,5 +27,7 @@ class UserProducts(models.Model):
     def add(user, product):
         UserProducts(
             user=user,
-            product=product
+            product=product,
+            status=UserProducts.PRODUCT_STATUS[0][0]
         ).save()
+
